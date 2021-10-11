c36c8f53fd5274cd545eaa4cd1a4d6b44abf862e
Yakub-Sadlil-Seyam <yakubsadlil@gmail.com>
2021-10-11 13:06:21 +0600

    init

---
+++ abc.py
@@ -0,0 +1,19 @@
+inp = input()
+print(inp[0])
+print(inp[1])
+
+print(len(inp))
+print("inp1")
+
+inp1 = input().split
+print(inp1)
+
+print("inp2")
+inp2 = input().split(" ")
+print(inp2)
+print(len(inp2))
+
+print("inp3")
+inp3 = input().split("@")
+print(inp3)
+print(len(inp3))
